3fb0425bcc4b08c45e4f05e3718df053dbb0b7ba
TWG2002123 <info@twiig.co.uk>
2025-04-13 12:21:29 +0700

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,12 @@
+# Print a greeting
+print("Hello, world!")
+# Variables
+name = "Thomas" # String
+age = 22 # Integer
+balance = 100.50 # Float
+is_student = True #Boolean
+
+print ("My name is", name)
+print ("I am", age, "years old")
+print ("My bank balance is $", balance)
+print ("Am I a student", is_student)
